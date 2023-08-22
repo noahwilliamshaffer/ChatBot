eea5ac6ba9853b7bc6b863db57ce7542b09de7e2
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2023-08-22 13:24:40 -0400

    generated a response while limiting the total chat history to 1000 tokens

--- ChatBot.py
+++ ChatBot.py
@@ -17,8 +17,12 @@ def get_chat_response(Text)
 for step in range(5):
     #encode the new user input, add the eos_token and return a tesor in pytorch
     new_user_input_ids = tokenizer.encode(input(">>user:")) + tokenizer.eos_token, return_tensor = 'pt')
+
     #append the new user iunput tokens to the chat history
     bot_input_ids = torch.cat([chat_history_ids, new_user_ids], dim = -1) if step > 0 else new_new_user_input_ids
+
+    #generated a response while limiting the total chat history to 1000 tokens
+    chat_history_ids = model.generate(bot_input_ids, max_length = 1000, pad_token_id=tokenizer.eos_token_id)
 
 
 
