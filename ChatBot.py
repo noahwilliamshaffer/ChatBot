c8d789b197c4e352f5841be0ddf8a5a92e60ff1c
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2023-08-22 12:39:15 -0400

    Added chat function to return user msg from html

--- ChatBot.py
+++ ChatBot.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index(): 
     return render_template('chat.html')
+
+@app.route("get", methods = ["GET","POST"]
+def chat():
+    msg = request.form("msg")
+    input = msg
+    return get_chat_response(input)
+
+
+    
