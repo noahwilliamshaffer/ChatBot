780016a3a5a0ba265b7e999e7d6dfa6c5d3fba55
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2023-08-22 13:42:35 -0400

    we are returning this response but we dont want to print it

--- ChatBot.py
+++ ChatBot.py
@@ -14,18 +14,20 @@ def chat():
 
 def get_chat_response(Text)
 #lets chat 5 lines
-for step in range(5):
-    #encode the new user input, add the eos_token and return a tesor in pytorch
-    new_user_input_ids = tokenizer.encode(input(">>user:")) + tokenizer.eos_token, return_tensor = 'pt')
 
-    #append the new user iunput tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_ids], dim = -1) if step > 0 else new_new_user_input_ids
+#we are returning this response but we dont want to print it
+    for step in range(5):
+        #encode the new user input, add the eos_token and return a tesor in pytorch
+        new_user_input_ids = tokenizer.encode(str(text)) + tokenizer.eos_token, return_tensor = 'pt')
 
-    #generated a response while limiting the total chat history to 1000 tokens
-    chat_history_ids = model.generate(bot_input_ids, max_length = 1000, pad_token_id=tokenizer.eos_token_id)
+        #append the new user iunput tokens to the chat history
+        bot_input_ids = torch.cat([chat_history_ids, new_user_ids], dim = -1) if step > 0 else new_user_input_ids
 
-    #pretty print last output tokens from bot
-    print("DialoGPT: {}").format((tokenizer.decode(chat_history_ids[:,bot_input_ids[-1]:][0],skip_special_tokens=true)))
+        #generated a response while limiting the total chat history to 1000 tokens
+        chat_history_ids = model.generate(bot_input_ids, max_length = 1000, pad_token_id=tokenizer.eos_token_id)
+
+        #pretty print last output tokens from bot
+        return tokenizer.decode(chat_history_ids[:,bot_input_ids[-1]:][0],skip_special_tokens=True)
 
 
 
